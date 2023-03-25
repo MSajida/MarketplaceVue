97d99b4fb71b8547cde24676e015e75b42f8632f
Sajida Mohammad <sajida.mohammad2093@gmail.com>
2023-03-25 12:34:12 -0500

    Update resetPassword.vue

--- src/components/resetPassword.vue
+++ src/components/resetPassword.vue
@@ -55,7 +55,7 @@
 
         console.log('password object', JSON.stringify(this.password))
         this.$axios
-          .post("http://localhost:8082/api/password/reset",this.password)
+          .post("http://100.26.237.185:8082/api/password/reset",this.password)
           .then((res) => {
             if (res.status == 200) {
               alert('Successfully changed the password')
