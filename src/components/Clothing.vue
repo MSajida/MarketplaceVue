68868380dd8d34e917a10807073ccfa690f22214
Sajida Mohammad <sajida.mohammad2093@gmail.com>
2023-03-25 19:02:12 -0500

    Update Clothing.vue

--- src/components/Clothing.vue
+++ src/components/Clothing.vue
@@ -147,7 +147,7 @@ export default ({
             console.log(this.Clothing, 'Clothing object');
             this.formdata.append('clothing', JSON.stringify(this.Clothing));
             this.$axios
-                .post("http://100.26.237.185:8082/clothing/addProduct", this.formdata)
+                .post("http://52.22.24.58:8082/clothing/addProduct", this.formdata)
                 .then((res) => {
                     if (res.status == 200) {
                         console.log("success")
