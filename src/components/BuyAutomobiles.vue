1f548dd172d64329629240faeba31892a2f2dd2a
Sajida Mohammad <sajida.mohammad2093@gmail.com>
2023-04-07 19:46:15 -0500

    Update BuyAutomobiles.vue

--- src/components/BuyAutomobiles.vue
+++ src/components/BuyAutomobiles.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="page-content page-container" id="page-content" style="background-color: azure;">
+    <div class="page-content page-container" id="page-content">
 
         <h1>Automobiles List</h1>
 
