b6b40656b2987d9ad41db0fb517a624c046c3f0b
Sajida Mohammad <sajida.mohammad2093@gmail.com>
2023-04-07 19:46:32 -0500

    Update BuyClothing.vue

--- src/components/BuyClothing.vue
+++ src/components/BuyClothing.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="page-content page-container" id="page-content" style="background-color: azure;">
+    <div class="page-content page-container" id="page-content">
 
         <h1>Clothing List</h1>
 
