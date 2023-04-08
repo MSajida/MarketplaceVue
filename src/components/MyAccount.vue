e98de507d6c7c1d0ef50f1d5dff9d9dfcf7d3649
Sajida Mohammad <sajida.mohammad2093@gmail.com>
2023-04-07 19:48:44 -0500

    Update MyAccount.vue

--- src/components/MyAccount.vue
+++ src/components/MyAccount.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="background" style="background-color: azure; height: 600px;">
+  <div>
     <b-container id="mainContainer" align-v="center">
 
       <b-row align-v="center" align-h="center">
